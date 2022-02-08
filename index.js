b0d32f29635290c70354c41a054e6429e05abf90
Zi Yang <yz186337@163.com>
2022-02-08 17:28:51 +0800

    add getPermutations method

--- index.js
+++ index.js
@@ -70,6 +70,33 @@ function MyLodash() {
   this.descendSort = function(arr) {
     return arr.sort((a, b) => b - a);
   }
+  /**
+   * get permutations of provided array
+   * 
+   * @param {*} arr 
+   * @returns array of permutations
+   */
+  this.getPermutations = function(arr) {
+    let permArr = [],
+        usedChars = [];
+
+    function permute(input) {
+      let i, ch;
+      for (i = 0; i < input.length; i++) {
+        ch = input.splice(i, 1)[0];
+        usedChars.push(ch);
+        if (input.length == 0) {
+          permArr.push(usedChars.slice());
+        }
+        permute(input);
+        input.splice(i, 0, ch);
+        usedChars.pop();
+      }
+      return permArr
+    };
+
+    return permute(arr);
+  }
 }
 
 const myLodash = new MyLodash();
